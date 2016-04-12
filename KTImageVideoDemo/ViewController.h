97b9c1a44ed57b94642f2b4700604a96b546c85b
Kevin Tu <819931323@qq.com>
2016-04-13 05:10:51 +0800

    Initial Commit

---
+++ KTImageVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTImageVideoDemo
+//
+//  Created by tujinqiu on 16/4/13.
+//  Copyright © 2016年 tujinqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KTImageVideoDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KTImageVideoDemo
+//
+//  Created by tujinqiu on 16/4/13.
+//  Copyright © 2016年 tujinqiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
